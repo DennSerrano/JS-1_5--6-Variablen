c3ff84317a0c7d56e6b0e08646080599e6098308
Dennserrano <dennserrano@gmail.com>
2021-10-25 14:13:12 +0200

    LevVariablen

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+/*let carName = "BMW"
+console.log(carName)
+
+let x = 150
+console.log(x)
+
+let y = 150
+console.log(y)
+
+let z = x+y
+console.log(z)
+
+let combiNation= "John" + "Doe" + 35
+console.log(combiNation)
+
+let firstName = "John"
+let lastName = "Doe " 
+let age = 35
+console.log(firstName + " " + lastName  + age)*/
+
+
+
+var z = 34;
+z = 67
+console.log(z)
+
+let x = 34
+x = 67
+console.log(x)
+
+
+const y = 34
+console.log(y)
